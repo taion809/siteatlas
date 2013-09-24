414d1e4b8bfad62ac935e6be06c07bfa4f260b9e
Nicholas J <nicholas.a.johns5@gmail.com>
2013-09-24 01:48:23 +0000

    Update for testing

---
+++ tests/Johnsn/Siteatlas/SiteatlasTest.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: nick
+ * Date: 9/24/13
+ * Time: 10:16 AM
+ * To change this template use File | Settings | File Templates.
+ */
+
+namespace Johnsn\Siteatlas;
+
+use \PHPUnit_Framework_TestCase;
+
+class SiteatlasTest extends PHPUnit_Framework_TestCase
+{
+    
+}
