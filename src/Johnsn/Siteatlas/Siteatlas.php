960612f324b9bc36e222303b306534b06082fbd2
Nicholas J <nicholas.a.johns5@gmail.com>
2013-09-26 09:55:01 +0000

    Update tests

--- src/Johnsn/Siteatlas/Siteatlas.php
+++ src/Johnsn/Siteatlas/Siteatlas.php
@@ -51,7 +51,8 @@ class Siteatlas
         $url_element->appendChild($this->sitemap->createElement('changefreq', $frequency));
         $url_element->appendChild($this->sitemap->createElement('priority', $priority));
 
-        $this->sitemap->appendChild($url_element);
+        $urlset = $this->sitemap->getElementsByTagName('urlset')->item(0);
+        $urlset->appendChild($url_element);
 
         return $url_element;
     }
