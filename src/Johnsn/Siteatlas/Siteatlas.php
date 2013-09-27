ac694b391bab4f7be354bedb945609c6b4cf87bd
Nicholas J <nicholas.a.johns5@gmail.com>
2013-09-27 03:43:08 +0000

    Updating stuff.. nondescript items

--- src/Johnsn/Siteatlas/Siteatlas.php
+++ src/Johnsn/Siteatlas/Siteatlas.php
@@ -9,67 +9,11 @@
 
 namespace Johnsn\Siteatlas;
 
-use DOMDocument;
-
 class Siteatlas
 {
-    protected $sitemap = null;
-
-    protected $urlset_namespace = "http://www.sitemaps.org/schemas/sitemap/0.9";
-
-    public function __construct(DOMDocument $sitemap = null, $version = '1.0', $encoding = 'UTF-8')
+    public function __construct()
     {
-        if(empty($sitemap)) {
-            $this->sitemap = new DOMDocument($version, $encoding);
-            $urlset = $this->sitemap->createElementNS($this->urlset_namespace, "urlset", '');
 
-            $this->sitemap->appendChild($urlset);
-        } else {
-            $this->sitemap = $sitemap;
-        }
-    }
-
-    public function getSitemapDocument()
-    {
-        return $this->sitemap;
-    }
-
-    public function load($filename, $options = 0)
-    {
-        //I don't like using @ for anything
-        //TODO: think of a better way.
-        $result = @$this->sitemap->load($filename, $options);
-
-        return $result;
-    }
-
-    public function addElement($location, $date, $priority = "0.5", $frequency = "weekly")
-    {
-        $url_element = $this->sitemap->createElement('url');
-        $url_element->appendChild($this->sitemap->createElement('loc', htmlentities($location)));
-        $url_element->appendChild($this->sitemap->createElement('lastmod', $date));
-        $url_element->appendChild($this->sitemap->createElement('changefreq', $frequency));
-        $url_element->appendChild($this->sitemap->createElement('priority', $priority));
-
-        $urlset = $this->sitemap->getElementsByTagName('urlset')->item(0);
-        $urlset->appendChild($url_element);
-
-        return $url_element;
-    }
-
-    public function save($filename, $format = true)
-    {
-        $this->sitemap->formatOutput = $format;
-        $this->sitemap->C14N(true, true);
-
-        $result = $this->sitemap->saveXML();
-
-        if(!empty($result))
-        {
-            $result = file_put_contents($filename, $result);
-        }
-
-        return $result;
     }
 
 }
--- tests/Johnsn/Siteatlas/Entity/SitemapTest.php
+++ tests/Johnsn/Siteatlas/Entity/SitemapTest.php
@@ -10,9 +10,19 @@
 namespace Johnsn\Siteatlas\Entity;
 
 use \PHPUnit_Framework_TestCase;
+use org\bovigo\vfs\vfsStream;
 
 class SitemapTest extends PHPUnit_Framework_TestCase
 {
+    private $filesystem;
+
+    public function setUp()
+    {
+        $this->filesystem = vfsStream::setup('mockDir');
+        $this->addValidSitemapData();
+        $this->addInvalidSitemapData();
+    }
+
     public function testGetSitemapReturnsDOMDocument()
     {
         $sitemap = new Sitemap();
@@ -75,6 +85,22 @@ class SitemapTest extends PHPUnit_Framework_TestCase
         $this->assertXmlStringEqualsXmlString($expected, $result);
     }
 
+    public function testSitemapLoadActuallyLoads()
+    {
+        $sitemap = new Sitemap();
+        $result = $sitemap->load(vfsStream::url('mockDir').DIRECTORY_SEPARATOR."mockmap.xml");
+
+        $this->assertTrue($result);
+    }
+
+    public function testSitemapLoadNonXml()
+    {
+        $sitemap = new Sitemap();
+        $result = $sitemap->load(vfsStream::url('mockDir').DIRECTORY_SEPARATOR."invalid_mockmap.xml");
+
+        $this->assertFalse($result);
+    }
+
     private function getBlankSitemap()
     {
         $xml = '<?xml version="1.0" encoding="UTF-8"?>';
@@ -99,5 +125,31 @@ class SitemapTest extends PHPUnit_Framework_TestCase
         $xml .= '</urlset>' . "\n";
 
         return $xml;
+    }
+
+    private function addValidSitemapData()
+    {
+        $xml  = '<?xml version="1.0" encoding="UTF-8"?>' . "\n";
+        $xml .= '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">' . "\n";
+        $xml .= "\t" . '<url>' . "\n";
+        $xml .= "\t\t" . '<loc>http://www.example.com/</loc>' . "\n";
+        $xml .= "\t\t" . '<lastmod>2005-01-01</lastmod>' . "\n";
+        $xml .= "\t\t" . '<changefreq>monthly</changefreq>' . "\n";
+        $xml .= "\t\t" . '<priority>0.8</priority>' . "\n";
+        $xml .= "\t" . '</url>' . "\n";
+        $xml .= '</urlset>' . "\n";
+
+        if(!file_exists(vfsStream::url('mockDir').DIRECTORY_SEPARATOR."mockmap.xml"))
+        {
+            file_put_contents(vfsStream::url('mockDir').DIRECTORY_SEPARATOR.'mockmap.xml', $xml);
+        }
+    }
+
+    private function addInvalidSitemapData()
+    {
+        if(!file_exists(vfsStream::url('mockDir').DIRECTORY_SEPARATOR."invalid_mockmap.xml"))
+        {
+            file_put_contents(vfsStream::url('mockDir').DIRECTORY_SEPARATOR.'invalid_mockmap.xml', 'Invalid Data');
+        }
     }
 }
